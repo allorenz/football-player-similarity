421315d7b7ebac5e15b27fafb865befc9455f375
allorenz <alexander.lorenz99@gmail.com>
2024-03-24 17:16:10 +0100

    script for utility functions

--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,6 @@
+import re
+
+
 # add mio and k to values 
 def adjust_money_appearance(x):
     x = int(x)
@@ -10,3 +13,15 @@ def adjust_money_appearance(x):
         x = "€" + str(x) + "k"
         return x
     return "€" + str(x)
+
+
+# Define a regular expression pattern
+pattern = r'\s([A-Z]+)$'
+
+# Function to clean the column
+def clean_nation(nation):
+    match = re.search(pattern, str(nation))
+    if match:
+        return match.group(1)
+    else:
+        return nation
