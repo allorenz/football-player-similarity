ea3e776ba389a4c2d69efd26570c91552914e8d2
allorenz <alexander.lorenz99@gmail.com>
2025-03-01 18:26:44 +0100

    update

--- src/dataloader.py
+++ src/dataloader.py
@@ -2,17 +2,24 @@ import os
 import pandas as pd
 from statsbombpy import sb
 from tqdm import tqdm
+from pathlib import Path
+from datetime import datetime
 
 
 COMPETITION_ID = 9
 SEASON_ID = 27
+PROJECT_ROOT_DIR = Path(__file__).parent.parent
 
+
+"""
+Old function:
 
 def load_data(frac=0.001):
     if(os.path.exists("./data/bundesliga.csv")):
         file_path = "./data/bundesliga.csv"
     elif (os.path.exists("../data/bundesliga.csv")):
         file_path = "../data/bundesliga.csv"
+    
     else:
         print("File does not exist, downloading data...")
         df = pd.DataFrame()
@@ -36,11 +43,48 @@ def load_data(frac=0.001):
     except Exception as e:
         print(f"Error reading the file: {e}") 
 
+"""
+
+def log_step(message):
+    timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    print(f"[{timestamp}] {message}")
+
+
+def load_data(frac=0.001):
+    if(os.path.exists((PROJECT_ROOT_DIR / "data" / "bundesliga.csv"))):
+        file_path = f"{PROJECT_ROOT_DIR}/data/bundesliga.csv" # PROJECT_ROOT_DIR / "data"
+        log_step("File exists")
+    else:
+        log_step("File does not exist, downloading data...")
+        df = pd.DataFrame()
+        df_matches = sb.matches(competition_id=COMPETITION_ID, season_id=SEASON_ID)
+        match_ids = df_matches["match_id"].to_list()
+
+        for match_id in tqdm(match_ids, desc="Concatenating matches", unit="match"):
+            events = sb.events(match_id=match_id)
+            df = pd.concat([df, events], ignore_index=True)
+
+        log_step(f"Data concatenated, resulting shape: {df.shape}")
+
+        file_path = f"{PROJECT_ROOT_DIR}/data/bundesliga.csv"
+        log_step(f"Writing to {file_path}")
+        df.to_csv(file_path, index=False)
+        return df
+    
+    log_step("Loading data form local file system")
+    try:
+        df = pd.read_csv(file_path).sample(frac=frac, random_state=42)
+        log_step(f"File loaded with shape: {df.shape}")
+        return df
+    except Exception as e:
+        log_step(f"Error reading the file: {e}") 
+
+    log_step("Done")
 
 
 def main():   
-    df = load_data(frac=0.0001)
-    print(df.shape)
+    df = load_data(frac=0.01)
+    print(df.dtypes)
     
 
 if __name__=="__main__":
