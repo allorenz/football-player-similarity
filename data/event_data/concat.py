19931dce39f42e20701b5feae5e667144e090245
allorenz <alexander.lorenz99@gmail.com>
2025-08-14 15:30:00 +0200

    update

---
+++ data/event_data/concat.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+if __name__ == "__main__":
+    leagues = ["bundesliga", "premier_league", "la_liga", "serie_a", "ligue_1", "ucl"]
+    dfs = []
+    for league in leagues:
+        print(league)
+        temp_df = pd.read_csv(f"{league}.csv")
+        temp_df["competition"] = league
+        dfs.append(temp_df)
+
+    df_concat = pd.concat(dfs, ignore_index=True)
+    df_concat.to_csv("../new_approach/all_leagues.csv", index=False)
+    df_concat.to_parquet("../new_approach/all_leagues.parquet", index=False)
